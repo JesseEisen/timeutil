213880ffbc10300da7288ee8ac8794a025973fff
JesseChen <lkchan0719@gmail.com>
2018-07-05 19:26:07 +0800

    fix format

--- test.c
+++ test.c
@@ -1,16 +1,16 @@
 #include "timeutil.h"
 
 int
-main(void)
+main (void)
 {
-	printf("\n");
-    printf("\tYMD: %26s\n", ymd());
-	printf("\tHMS: %24s\n", hms());
-    printf("\ttimestamp: %23lld\n", timestamp());
-    sleep(2);
-    printf("\ttimstamp(2s later): %14lld\n", timestamp());
-	printf("\tstring stamp: %22s\n", timestampstr());
-	printf("\n");
+  printf ("\n");
+  printf ("\tYMD: %26s\n", ymd ());
+  printf ("\tHMS: %24s\n", hms ());
+  printf ("\ttimestamp: %23lld\n", timestamp ());
+  sleep (2);
+  printf ("\ttimstamp(2s later): %14lld\n", timestamp ());
+  printf ("\tstring stamp: %22s\n", timestampstr ());
+  printf ("\n");
 
-    return 0;
+  return 0;
 }
