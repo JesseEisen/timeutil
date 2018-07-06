8fbb5293d25360e810e2dc66b51af4fee80dd490
JesseChen <lkchan0719@gmail.com>
2018-07-06 17:57:15 +0800

    add timer wraped by timer_create function

---
+++ ctimer.h
@@ -0,0 +1,13 @@
+#ifndef _CTIMER_H_
+#define _CTIMER_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <signal.h>
+#include <errno.h>
+
+#endif
