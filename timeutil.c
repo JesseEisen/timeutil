6c52c57950d63af0d353c1b20447b200bf24ae39
JesseChen <lkchan0719@gmail.com>
2018-07-06 12:22:14 +0800

    format code with astyle

--- timeutil.c
+++ timeutil.c
@@ -1,7 +1,6 @@
 #include "timeutil.h"
 
-static char result[3 + 1 + 3 + 1 + 20 + 1 + 20 + 1 + 20 + 1 + 20 + 1 + 20 +
-		   1 + 1];
+static char result[3 + 1 + 3 + 1 + 20 + 1 + 20 + 1 + 20 + 1 + 20 + 1 + 20 + 1 + 1];
 static char strts[30];
 
 #ifdef CLOCK_MONOTONIC_RAW
@@ -13,57 +12,57 @@ static char strts[30];
 int64_t
 timestampwall (void)
 {
-  struct timespec ts;
-  clock_gettime (CLOCKMONO, &ts);
-  return (int64_t) (ts.tv_sec + 1e-9 * ts.tv_nsec);
+	struct timespec ts;
+	clock_gettime (CLOCKMONO, &ts);
+	return (int64_t) (ts.tv_sec + 1e-9 * ts.tv_nsec);
 }
 
 int64_t
 timestampcpu (void)
 {
-  struct timespec ts;
-  clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &ts);
-  return (int64_t) (ts.tv_sec + 1e-9 * ts.tv_nsec);
+	struct timespec ts;
+	clock_gettime (CLOCK_PROCESS_CPUTIME_ID, &ts);
+	return (int64_t) (ts.tv_sec + 1e-9 * ts.tv_nsec);
 }
 
 int64_t
 timestamp (void)
 {
-  struct timeval tv;
-  int ret;
+	struct timeval tv;
+	int ret;
 
-  ret = gettimeofday (&tv, NULL);
-  if (ret == -1)
-    return -1;
-  return (int64_t) ((int64_t) tv.tv_sec * 1000 + (int64_t) tv.tv_usec / 1000);
+	ret = gettimeofday (&tv, NULL);
+	if (ret == -1)
+		return -1;
+	return (int64_t) ((int64_t) tv.tv_sec * 1000 + (int64_t) tv.tv_usec / 1000);
 }
 
 char *
 ymd (void)
 {
-  struct timeval tv;
-  int ret;
+	struct timeval tv;
+	int ret;
 
-  ret = gettimeofday (&tv, NULL);
-  if (ret == -1)
-    return NULL;
+	ret = gettimeofday (&tv, NULL);
+	if (ret == -1)
+		return NULL;
 
-  strftime (result, sizeof (result), "%Y.%m.%d", localtime (&tv.tv_sec));
-  return result;
+	strftime (result, sizeof (result), "%Y.%m.%d", localtime (&tv.tv_sec));
+	return result;
 }
 
 char *
 hms (void)
 {
-  struct timeval tv;
-  int ret;
+	struct timeval tv;
+	int ret;
 
-  ret = gettimeofday (&tv, NULL);
-  if (ret == -1)
-    return NULL;
+	ret = gettimeofday (&tv, NULL);
+	if (ret == -1)
+		return NULL;
 
-  strftime (result, sizeof (result), "%T", localtime (&tv.tv_sec));
-  return result;
+	strftime (result, sizeof (result), "%T", localtime (&tv.tv_sec));
+	return result;
 
 }
 
@@ -71,15 +70,15 @@ hms (void)
 char *
 timestampstr (void)
 {
-  struct timeval tv;
-  int ret;
+	struct timeval tv;
+	int ret;
 
-  ret = gettimeofday (&tv, NULL);
-  if (ret == -1)
-    return NULL;
+	ret = gettimeofday (&tv, NULL);
+	if (ret == -1)
+		return NULL;
 
-  strftime (strts, sizeof (strts), "%T", localtime (&tv.tv_sec));
-  snprintf (strts + 8, sizeof (strts) - 8, ".%6ld", tv.tv_usec);
+	strftime (strts, sizeof (strts), "%T", localtime (&tv.tv_sec));
+	snprintf (strts + 8, sizeof (strts) - 8, ".%6ld", tv.tv_usec);
 
-  return strts;
+	return strts;
 }
